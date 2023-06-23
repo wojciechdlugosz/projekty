66d098b570f7886bd762dacaff3da15544c1b1bd
Wojciech Dlugosz <webmaster@styronet.pl>
2023-06-23 14:12:51 +0200

    scroller project

---
+++ javascript/scroller-project/index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const scroller = new Scroller('#root');
+
+    document.addEventListener('wheel', (event) => scroller.listenScroll(event));
+    document.addEventListener('swipeUp', () => scroller.scroll(1));
+    document.addEventListener('swipeDown', () => scroller.scroll(-1));
+    document.addEventListener('keydown', (event) => {
+    switch (event.keyCode) {
+      case 40:
+        return scroller.scroll(1)
+      case 38:
+        return scroller.scroll(-1)
+
+      default:
+        return;
+    }
+  })
+
+ 
+})
