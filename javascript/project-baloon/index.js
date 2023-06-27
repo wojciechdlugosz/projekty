114258ad1e3cab87a873235d2c89ae1d799ed3ea
Wojciech Dlugosz <webmaster@styronet.pl>
2023-06-27 18:35:29 +0200

    project ballon

---
+++ javascript/project-baloon/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const animBg = new AnimBg('.anim-bg')
+
+  document.addEventListener('mousemove', (event) => animBg.listenCursorMove(event))
+})
